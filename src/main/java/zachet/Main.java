e26a6215c0e48124b30d2a5b8a00b8e8007be23f
mashakryuchkova <masha.kryuchkova17@mail.ru>
2020-06-01 16:49:16 +0300

    tasks

--- src/main/java/zachet/Main.java
+++ src/main/java/zachet/Main.java
@@ -1,4 +1,23 @@
 package zachet;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int p = s.nextInt();
+        int q = s.nextInt();
+        int arr[][] = new int[p][q];
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                arr[i][j] = s.nextInt();
+            }
+        }
+        for (int i = 0; i < q; i++) {
+            for (int j = p - 1; j >= 0; j--) {
+                System.out.print(arr[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
